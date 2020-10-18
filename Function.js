f67f1f334f90c7e424f16b5d62ea160b4219329c
raccoon <manuelblock@outlook.com>
2020-10-17 22:03:46 -0300

    remove button on click

---
+++ Function.js
@@ -0,0 +1,8 @@
+function remove() {
+    var elem = document.getElementById('yes');
+    elem.parentNode.removeChild(elem);
+    return false;
+    var elem = document.getElementById('no');
+    elem.parentNode.removeChild(elem);
+    return false;
+}
