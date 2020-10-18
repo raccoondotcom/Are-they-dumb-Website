003526b8f5a07e55bb69711fc3ba1653fb10912a
raccoon <manuelblock@outlook.com>
2020-10-17 23:57:10 -0300

    test

--- Function.js
+++ Function.js
@@ -1,10 +1,8 @@
 function remove() {
     var elem = document.getElementById('no');
     elem.parentNode.removeChild(elem);
-    return false;
 }
 function removeyes() {
     var elem = document.getElementById('yes');
     elem.parentNode.removeChild(elem);
-    return false;
 }
