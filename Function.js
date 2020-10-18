33e13a07acdc039ce397e4feec7fb6a2d97bca8d
Wisse <wissehes@gmail.com>
2020-10-18 15:30:24 +0200

    replaced remove functions with one function

--- Function.js
+++ Function.js
@@ -1,10 +1,18 @@
 function remove() {
-    var elem = document.getElementById('no');
-    elem.parentNode.removeChild(elem);
-    return false;
+  var elem = document.getElementById("no");
+  elem.parentNode.removeChild(elem);
+  return false;
 }
 function removeyes() {
-    var elem = document.getElementById('yes');
-    elem.parentNode.removeChild(elem);
-    return false;
+  var elem = document.getElementById("yes");
+  elem.parentNode.removeChild(elem);
+  return false;
+}
+
+function removeButtons() {
+  const yesButton = document.getElementById("yes");
+  const noButton = document.getElementById("no");
+
+  yesButton.remove();
+  noButton.remove();
 }
